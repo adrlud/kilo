1d549d0629c862c586668e67ffb5ede2c272d5e3
Ludvigsson <adrian.ludvigsson@gmail.com>
2019-02-11 12:12:48 +0100

    Step 52-54
    Page up and page down do not work. Delete works but funktion have not been implemented

--- kilo.c
+++ kilo.c
@@ -17,6 +17,9 @@ enum editorKey{
   ARROW_RIGHT,
   ARROW_UP,
   ARROW_DOWN,
+  DEL_KEY,
+  HOME_KEY,
+  END_KEY,
   PAGE_UP,
   PAGE_DOWN
 };
@@ -78,8 +81,13 @@ int editorReadKey() {
         if (read(STDIN_FILENO, &seq[2], 1) != 1) return '\x1b';
         if (seq[2] == '~') {
           switch (seq[1]) {
+            case '1': return HOME_KEY;
+            case '3': return DEL_KEY;
+            case '4': return END_KEY;
             case '5': return PAGE_UP;
             case '6': return PAGE_DOWN;
+            case '7': return HOME_KEY;
+            case '8': return END_KEY;
           }
         }
       } else {
@@ -88,9 +96,17 @@ int editorReadKey() {
           case 'B': return ARROW_DOWN;
           case 'C': return ARROW_RIGHT;
           case 'D': return ARROW_LEFT;
+          case 'H': return HOME_KEY;
+          case 'F': return END_KEY;
         }
       }
+    } else if (seq[0] == '0'){
+      switch(seq[1]) {
+        case 'H': return HOME_KEY;
+        case 'F': return END_KEY;
+      }
     }
+
     return '\x1b';
   } else {
     return c;
@@ -223,7 +239,19 @@ void editorProcessKeypress() {
         write(STDOUT_FILENO, "\x1b[H", 3);
         exit(0);
         break;
+    
+    case HOME_KEY:
+      E.cx = 0;
+      break;
 
+    case END_KEY:
+      E.cx = E.screencols - 1;
+      break;
+    
+    case DEL_KEY:
+      break;
+
+    /* fungerar inte*/
     case PAGE_UP:
     case PAGE_DOWN:
       {
