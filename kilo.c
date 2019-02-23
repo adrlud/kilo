80a55fc8de5b6af83ca03334baf957fe32218f96
Ludvigsson <adrian.ludvigsson@gmail.com>
2019-02-23 19:28:00 +0100

    Step 115-125
    Backspace and Enter button

--- kilo.c
+++ kilo.c
@@ -15,7 +15,12 @@
 #include <fcntl.h>
 #include <stdarg.h>
 
-
+typedef struct erow{
+  int size;
+  int rsize;
+  char *chars;
+  char *render;
+} erow;
 
 /*** defines ***/
 
@@ -23,10 +28,12 @@
 
 #define KILO_VERISION "0.0.1"
 #define KILO_TAB_STOP 8
+#define KILO_QUIT_TIMES 3
 
 /*** prototypes ***/
 
 void editorSetStatusMessage(const char *fmt, ...);
+void editorRowDelChar(erow *row, int at);
 
 enum editorKey{
   BACKSPACE = 127,
@@ -42,12 +49,7 @@ enum editorKey{
 };
 
 /*** data ***/
-typedef struct erow{
-  int size;
-  int rsize;
-  char *chars;
-  char *render;
-} erow;
+
 
 struct editorConfig {
     int cx, cy; 
@@ -208,21 +210,33 @@ void editorUpdateRow(erow *row){
   row->rsize = idx;
 }
 
-void editorAppendRow(char *s, size_t len){
-  E.row = realloc(E.row, sizeof(erow) * (E.numrows+1));
-  int at = E.numrows;
-
+void editorInsertRow(int at, char *s, size_t len) {
+  if (at < 0 || at > E.numrows) return;
+  E.row = realloc(E.row, sizeof(erow) * (E.numrows + 1));
+  memmove(&E.row[at + 1], &E.row[at], sizeof(erow) * (E.numrows - at));
+ 
   E.row[at].size = len;
   E.row[at].chars = malloc(len + 1);
   memcpy(E.row[at].chars, s, len);
   E.row[at].chars[len] = '\0';
-
   E.row[at].rsize = 0;
   E.row[at].render = NULL;
   editorUpdateRow(&E.row[at]);
-  
-  E.dirty++;
   E.numrows++;
+  E.dirty++;
+}
+
+void editorFreeRow(erow *row){
+  free(row->render);
+  free(row->chars);
+}
+
+void editorDelRow(int at){
+  if (at < 0 || at >= E.numrows) return;
+  editorFreeRow(&E.row[at]);
+  memmove(&E.row[at], &E.row[at + 1], sizeof(erow) * (E.numrows - at - 1));
+  E.numrows--;
+  E.dirty++;
 }
 
 void editorRowInsertChar(erow *row, int at, int c){
@@ -235,12 +249,63 @@ void editorRowInsertChar(erow *row, int at, int c){
   E.dirty++;
 }
 
+void editorRowAppendString(erow *row, char *s, size_t len){
+  row->chars = realloc(row->chars, row->size + len + 1);
+  memcpy(&row->chars[row->size], s, len);
+  row->size += len;
+  row->chars[row->size] = '\0';
+  editorUpdateRow(row);
+  E.dirty++;
+
+}
+/*** editor operations ***/
+
 void editorInsertChar(int c){
   if(E.cy == E.numrows){
-    editorAppendRow("", 0);
+    editorInsertRow(E.numrows,"", 0);
   }
+
   editorRowInsertChar(&E.row[E.cy], E.cx, c);
   E.cx++;
+}
+
+void editorInsertNewline(){
+  if (E.cx==0){
+    editorInsertRow(E.cy,"",0);
+  } else {
+    erow *row = &E.row[E.cy];
+    editorInsertRow(E.cy + 1, &row->chars[E.cx], row->size - E.cx);
+    row = &E.row[E.cy];
+    row->size = E.cx;
+    row->chars[row->size] = '\0';
+    editorUpdateRow(row);
+  }
+  E.cy++;
+  E.cx = 0;
+}
+
+void editorDelChar(){
+  if(E.cy == E.numrows) return;
+  if(E.cx == 0 && E.cy == 0) return;
+
+  erow *row = &E.row[E.cy];
+  if(E.cx > 0 ){
+    editorRowDelChar(row, E.cx - 1);
+    E.cx--;
+  } else {
+    E.cx = E.row[E.cy-1].size;
+    editorRowAppendString(&E.row[E.cy-1], row->chars, row->size);
+    editorDelRow(E.cy);
+    E.cy--;
+  }
+}
+
+void editorRowDelChar(erow *row, int at){
+  if( at < 0 || at >= row->size) return;
+  memmove(&row->chars[at], &row->chars[at+1], row->size - at);
+  row->size--;
+  editorUpdateRow(row);
+  E.dirty++;
 }
 
 /*** file i/o ***/
@@ -278,7 +343,7 @@ void editorOpen(char *filename){
     while((linelen = getline(&line, &linecap, fp)) != -1){ 
       while(linelen > 0 && (line[linelen-1] == '\n' || line[linelen-1] == '\r'))
         linelen--;
-      editorAppendRow(line, linelen);
+      editorInsertRow(E.numrows,line, linelen);
   }
   free(line);
   fclose(fp);
@@ -487,17 +552,27 @@ void editorMoveCursor(int key) {
 }
 
 void editorProcessKeypress() {
+  static int quit_times = KILO_QUIT_TIMES;
+
+
   int c = editorReadKey();
   switch (c) {
     case '\r':
+      editorInsertNewline();
     break;
 
     case CTRL_KEY('q'):
+        if( E.dirty  && quit_times > 0){
+          editorSetStatusMessage("Warning! File has unsaved changes. Press Ctrl-Q %d more times to quit", quit_times );
+        quit_times--;
+        return;
+        }
+
         write(STDOUT_FILENO, "\x1b[2J", 4);
         write(STDOUT_FILENO, "\x1b[H", 3);
         exit(0);
         break;
-    
+        
     case CTRL_KEY('s'):
       editorSave();
       break;
@@ -514,6 +589,8 @@ void editorProcessKeypress() {
     case BACKSPACE:
     case CTRL_KEY('h'):
     case DEL_KEY:
+      if(c == DEL_KEY) editorMoveCursor(ARROW_RIGHT);
+      editorDelChar();
       /*TODO*/
       break;
 
@@ -547,6 +624,8 @@ void editorProcessKeypress() {
     default:
       editorInsertChar(c);
   }
+
+  quit_times = KILO_QUIT_TIMES;
 }
 
 
