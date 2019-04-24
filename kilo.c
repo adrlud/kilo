6ee8eda1200cdc5e29025a9c9e557b1eb52243f0
Ludvigsson <adrian.ludvigsson@gmail.com>
2019-04-24 16:57:44 +0200

    step 145 - 159

--- kilo.c
+++ kilo.c
@@ -53,10 +53,19 @@ enum editorKey{
 
 enum editorHighlight{
   HL_NORMAL = 0,
-  HL_NUMBER
+  HL_NUMBER,
+  HL_MATCH
 };
 
+#define HL_HIGHLIGHT_NUMBERS (1<<0)
+
 /*** data ***/
+
+struct editorSyntax {
+  char *filetype;
+  char **filematch;
+  int flags;
+};
 
 
 struct editorConfig {
@@ -72,9 +81,26 @@ struct editorConfig {
     int numrows;
     erow *row;
     int dirty;
+    struct editorSyntax *syntax;
     char *filename;
 };
 struct editorConfig E;
+
+/*** filetypes ***/
+
+char *C_HL_extensions[] = { ".c" , ".h", ".cpp", NULL};
+
+struct editorSyntax HLDB[] = { 
+  {
+    "c",
+    C_HL_extensions,
+    HL_HIGHLIGHT_NUMBERS
+  },
+};
+
+#define HLDB_ENTRIES (sizeof(HLDB) / sizeof([0]))
+
+
 
 /*** terminal ***/
 
@@ -183,6 +209,44 @@ int getWindowSize(int *rows, int *cols) {
   }
 }
 
+/*** syntax highligting ***/
+
+int is_separator(int c){
+  return isspace(c) || c == '\0' || strchr(",.()+-/*=~%<>[];", c) != NULL;
+
+}
+
+void editorUpdateSyntax(erow *row){
+  row->hl = realloc(row->hl, row->rsize);
+  memset(row->hl, HL_NORMAL, row->rsize);
+
+  int prev_sep = 1;
+  
+  int i = 0;
+  while (i < row->rsize){
+    char c = row->render[i];
+    unsigned char prev_hl = (i > 0) ? row->hl[i-1] : HL_NORMAL;
+
+    if((isdigit(c) && (prev_sep || prev_hl == HL_NUMBER)) || (c == '.' && prev_hl == HL_NUMBER)) {
+      row->hl[i] = HL_NUMBER;
+      i++;
+      prev_sep = 0;
+      continue;
+    } 
+
+    prev_sep = is_separator(c);
+    i++;
+  }
+}
+
+int editorSyntaxToColor(int hl){
+  switch( hl ){
+    case HL_NUMBER: return 31;
+    case HL_MATCH: return 34;
+    default: return 37;
+  }
+}
+
 /*** row operations ***/
 
 int editorRowCxToRx(erow *row, int cx){
@@ -231,6 +295,8 @@ void editorUpdateRow(erow *row){
   }
   row->render[idx] = '\0';
   row->rsize = idx;
+
+  editorUpdateSyntax(row);
 }
 
 void editorInsertRow(int at, char *s, size_t len) {
@@ -408,6 +474,15 @@ void editorSave(){
 void editorFindCallback(char *query, int key) {
   static int last_match = -1;
   static int direction = 1;
+
+  static int saved_hl_line;
+  static int *saved_hl = NULL;
+
+  if(saved_hl){
+    memcpy(E.row[saved_hl_line].hl, saved_hl, E.row[saved_hl_line].rsize);
+    free(saved_hl);
+    saved_hl = NULL;
+  }
   
   if (key == '\r' || key == '\x1b') {
     last_match = -1;
@@ -438,6 +513,11 @@ void editorFindCallback(char *query, int key) {
       E.cy = current;
       E.cx = editorRowRxToCx(row, match - row->render);
       E.rowoff = E.numrows;
+
+      saved_hl_line = current;
+      saved_hl = malloc(row->rsize);
+      memcpy(saved_hl, row->hl, row->rsize);
+      memset(&row->hl[match - row->render], HL_MATCH, strlen(query));
       break;
     }
   }
@@ -535,16 +615,29 @@ void editorDrawRows(struct abuf *ab) {
       if(len < 0 ) len = 0;
       if (len > E.screencols) len = E.screencols;
       char *c = &E.row[filerow].render[E.coloff];
+      unsigned char *hl = &E.row[filerow].hl[E.coloff];
+      int current_color = -1;
       int j;
       for (j = 0; j < len; j++){
-        if (isdigit(c[j])){
-          abAppend(ab, "\x1b[31m", 5);
+        if(hl[j] == HL_NORMAL){
+          if(current_color != -1){
+            abAppend(ab, "\x1b[39m", 5);
+            current_color = -1;
+          }
           abAppend(ab, &c[j], 1);
-          abAppend(ab, "\x1b[39m", 5);
         } else {
+          int color = editorSyntaxToColor(hl[j]);
+          if(color != current_color){
+            current_color = color;
+          char buf[16];
+          int clen = snprintf(buf, sizeof(buf), "\x1b[%dm", color);
+          abAppend(ab, buf, clen);
+          }
+          
           abAppend(ab, &c[j], 1);
         }
       }
+      abAppend(ab, "\x1b[39m", 5);
     }
     abAppend(ab, "\x1b[K", 3);
     abAppend(ab, "\r\n", 2);
@@ -781,6 +874,7 @@ void initEditor() {
   E.filename = NULL;
   E.statusmsg[0] = '\0';
   E.statusmsg_time = 0;
+  E.syntax = NULL;
 
   if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
   E.screenrows -= 2;
